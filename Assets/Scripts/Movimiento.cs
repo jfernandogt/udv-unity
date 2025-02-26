e29a4f0765dd335c746aeced79fd7880c8e9b735
Fernando Barrios <fer.b.1990@gmail.com>
2025-02-25 23:46:36 -0600

    feat: mejorar velocidad salto

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    [SerializeField] private float velInicialSalto = 24f;
     [SerializeField] private LayerMask capaDeSalto;
     [SerializeField] private float velocidadCaminata = 4f;
     [SerializeField] private float alturaSalto = 4f;
@@ -12,10 +11,12 @@ public class Movimiento : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+
     }
 
     public void Moverse(float movimientoX)
@@ -29,6 +30,8 @@ public class Movimiento : MonoBehaviour
 
         if (debeSaltar)
         {
+            float gravedad = Physics2D.gravity.y * rb.gravityScale;
+            float velInicialSalto = Mathf.Sqrt(2 * Mathf.Abs(gravedad) * alturaSalto);
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, velInicialSalto);
         }
     }
